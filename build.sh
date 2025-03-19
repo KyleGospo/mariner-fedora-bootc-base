dd48af904edcffc7a114685ed111d4d1727a0944
Kyle Gospodnetich <me@kylegospodneti.ch>
2025-03-19 10:10:51 -0700

    Install repo packages

--- build.sh
+++ build.sh
@@ -6,4 +6,16 @@ set -ouex pipefail
 rpm -e $(rpm -qa | grep fedora-) --nodeps
 rpm -i https://packages.microsoft.com/yumrepos/azurelinux-3.0-prod-base-x86_64/Packages/a/azurelinux-release-3.0-25.azl3.noarch.rpm
 
+dnf5 -y install \
+	azurelinux-repos \
+	azurelinux-repos-amd \
+	azurelinux-repos-extended \
+	azurelinux-repos-ms-non-oss \
+	azurelinux-repos-ms-oss \
+	azurelinux-repos-shared \
+	azurelinux-rpm-macros \
+	azurelinux-sysinfo \
+	azure-nvme \
+	azure-storage-cpp
+
 dnf5 -y update
