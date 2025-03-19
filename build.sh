c9c8200bc9c75d0a4cda344ba80a7e819a0326d9
Kyle Gospodnetich <me@kylegospodneti.ch>
2025-03-19 10:07:19 -0700

    Make it all of fedora

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 # Remove fedora-release packages (with nodeps), then immeidately install the mariner replacement
-rpm -e $(rpm -qa | grep fedora-release-) --nodeps
+rpm -e $(rpm -qa | grep fedora-) --nodeps
 rpm -i https://packages.microsoft.com/yumrepos/azurelinux-3.0-prod-base-x86_64/Packages/a/azurelinux-release-3.0-25.azl3.noarch.rpm
 
 dnf5 -y update
