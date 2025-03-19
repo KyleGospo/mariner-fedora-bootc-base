9fc9e8c133720a35719bcd6d6d2737a0365116d8
Kyle Gospodnetich <me@kylegospodneti.ch>
2025-03-19 12:38:29 -0700

    Replace kernel, rebuild initramfs

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ set -ouex pipefail
 rpm -ev $(rpm -qa | grep ^fedora-) --nodeps
 rpm -iv https://packages.microsoft.com/yumrepos/azurelinux-3.0-prod-base-x86_64/Packages/a/azurelinux-release-3.0-25.azl3.noarch.rpm
 
+# Install Azure Linux packages/repositories
+# Note that some cannot be installed due to bzip2 versioning differences breaking bootc at this time.
 dnf -y install \
 	azurelinux-repos \
 	azurelinux-repos-amd \
@@ -15,3 +17,24 @@ dnf -y install \
 	azurelinux-repos-shared \
 	azurelinux-rpm-macros \
 	azurelinux-sysinfo
+
+# Install Azure Linux kernel
+dnf -y remove --noautoremove \
+	kernel \
+	kernel-core \
+	kernel-modules \
+	kernel-modules-core \
+	kernel-modules-extra
+
+dnf -y install \
+	kernel \
+	kernel-drivers-gpu \
+	kernel-drivers-intree-amdgpu \
+	kernel-drivers-sound \
+	kernel-uki
+
+# Rebuild initramfs
+QUALIFIED_KERNEL="$(dnf repoquery --installed --qf '%{evr}.%{arch}' "kernel")"
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+
+chmod 0600 /usr/lib/modules/"$QUALIFIED_KERNEL"/initramfs.img
